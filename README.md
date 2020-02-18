212a1b85bce76dabe19f4e33e0e32acb6d7c01c9
Alessio Caradossi <acaradossi@hotels.com>
2020-02-18 14:32:53 +0100

    first naive implementation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # net_tracker
 simple python script to track local ip via mqtt
+
+under the [ips] section of the config.ini you can assign a name at each ip you want to be tracked
+
+foo=192.168.0.5
+bar=192.168.0.10
+and so on
+
+under [mqtt] section you should configure the connection to your mqtt server
+host=
+user=
+pwd= 
---
+++ net_tracker.py
@@ -0,0 +1,46 @@
+from paho.mqtt.client import Client
+
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return 1 if subprocess.call(command) == 0 else 0
+
+client = Client(client_id="net_tracker")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connesso con successo")
+
+def on_message(client, userdata, message):
+    message = message.payload.decode()
+    publish_topic = f'net_monitor/device/{message}'
+    print(publish_topic)
+    payload = 0
+    print(message)
+    if config['ips'][message]:
+        payload = ping(config['ips'][message])
+    print(payload)
+    client.publish(publish_topic, payload=payload, qos=2)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(config['mqtt']['user'], password=config['mqtt']['pwd'])
+client.connect(config['mqtt']['host'])
+client.subscribe("net_tracker/scan", qos=2)
+client.loop_forever()
